2d91ecffb1b508083391d812a293b26ded50f5b0
Oskar Szura <oskar.szura@gmail.com>
2020-11-09 14:33:13 +0100

    CODA-116 - Split with  delimiters

--- include/strings.h
+++ include/strings.h
@@ -3,22 +3,29 @@
 #include <vector>
 
 namespace coda {
-std::vector<std::string> split(std::string text, std::string delimiter) {
-  std::size_t start;
-  std::size_t end;
+std::vector<std::string> split(std::string text, std::string delimiter,
+                               int number = -1) {
   std::vector<std::string> split;
+  int i = 0;
+  std::size_t start = 0;
+  std::size_t end = text.find(delimiter);
 
-  start = 0;
-  end = text.find(delimiter);
   while (true) {
     split.push_back(text.substr(start, end - start));
 
-    if (end == std::string::npos) {
+    if (end == std::string::npos || (number != -1 && i >= number - 1)) {
       break;
     }
 
     start = end + 1;
-    end = text.find(delimiter, end + 1);
+
+    if (number == -1 || (number != -1 && i == number - 1)) {
+      end = text.find(delimiter, end + 1);
+    } else {
+      end = text.length();
+    }
+
+    i++;
   }
 
   return split;
--- include/strings_test.h
+++ include/strings_test.h
@@ -3,9 +3,24 @@
 #include <vector>
 
 TEST_CASE("split") {
-  std::vector<std::string> v = coda::split("Some text about it.", " ");
-  REQUIRE(v[0] == "Some");
-  REQUIRE(v[1] == "text");
-  REQUIRE(v[2] == "about");
-  REQUIRE(v[3] == "it.");
+  SECTION("should split string with all delimiters") {
+    std::vector<std::string> v = coda::split("Some text about it.", " ");
+    REQUIRE(v[0] == "Some");
+    REQUIRE(v[1] == "text");
+    REQUIRE(v[2] == "about");
+    REQUIRE(v[3] == "it.");
+  }
+
+  SECTION("should return one element when no delimiters are found") {
+    std::vector<std::string> v = coda::split("one", " ");
+    REQUIRE(v[0] == "one");
+    REQUIRE(v.size() == 1);
+  }
+
+  SECTION("should split for particular delimiter number") {
+    std::vector<std::string> v = coda::split("Some text about it.", " ", 2);
+    REQUIRE(v[0] == "Some");
+    REQUIRE(v[1] == "text about it.");
+    REQUIRE(v.size() == 2);
+  }
 }
